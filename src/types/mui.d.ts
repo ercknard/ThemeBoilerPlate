860526040440fc91a5a52ce556578bfba000df9d
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2026-09-03 19:27:31 +0800

    fix

--- src/types/mui.d.ts
+++ src/types/mui.d.ts
@@ -1,4 +1,33 @@
+import '@mui/material/styles';
 import '@mui/material/Typography';
+
+declare module '@mui/material/styles' {
+  interface TypographyVariants {
+    display: React.CSSProperties;
+    title: React.CSSProperties;
+    sectionTitle: React.CSSProperties;
+    lead: React.CSSProperties;
+    large: React.CSSProperties;
+    medium: React.CSSProperties;
+    small: React.CSSProperties;
+    label: React.CSSProperties;
+    overlineCustom: React.CSSProperties;
+    code: React.CSSProperties;
+  }
+
+  interface TypographyVariantsOptions {
+    display?: React.CSSProperties;
+    title?: React.CSSProperties;
+    sectionTitle?: React.CSSProperties;
+    lead?: React.CSSProperties;
+    large?: React.CSSProperties;
+    medium?: React.CSSProperties;
+    small?: React.CSSProperties;
+    label?: React.CSSProperties;
+    overlineCustom?: React.CSSProperties;
+    code?: React.CSSProperties;
+  }
+}
 
 declare module '@mui/material/Typography' {
   interface TypographyPropsVariantOverrides {
